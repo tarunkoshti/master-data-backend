e8eab16bb1e0667fadf547594fbc6250001dd4a1
Tarun Koshti <tarunkoshti910@gmail.com>
2026-07-15 12:14:01 +0530

    added check for type cities and states

--- src/modules/master-data/master-data.schema.js
+++ src/modules/master-data/master-data.schema.js
@@ -7,12 +7,28 @@ const masterDataBodySchema = z.object({
     value: z.string().optional(),
     name: z.string().min(1, 'Name is required'),
     parent_id: z.number().int().positive().optional().nullable(),
+}).refine(data => {
+    if (['cities', 'states'].includes(data.type)) {
+        return data.parent_id !== undefined && data.parent_id !== null;
+    }
+    return true;
+}, {
+    message: "parent_id is required when type is cities or states",
+    path: ["parent_id"]
 });
 
 export const getAllSchema = z.object({
     query: z.object({
         type: z.string().optional(),
         parent_id: z.string().optional(),
+    }).refine(data => {
+        if (['cities', 'states'].includes(data.type)) {
+            return data.parent_id !== undefined && data.parent_id !== null && data.parent_id !== '';
+        }
+        return true;
+    }, {
+        message: "parent_id is required when type is cities or states",
+        path: ["parent_id"]
     }),
 });
 
